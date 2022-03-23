3a3386aadb60d560f332bbb42e0c37a24e498664
Roy <40725093+roy1337@users.noreply.github.com>
2022-03-23 15:53:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+const APIURL = 'https:/api.github.com/users/'
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+/*
+The architecture:
+1. async functions to fetch (get) the data (get)
+2. functions to append (innerHTML, childappend) to the DOM
+*/
+
+
+async function getUser(username) {
+    try{
+        const {data} = await axios(APIURL + username)
+        createUserCard(data)
+        getRepos(username)
+    }
+    catch(err) {
+        if(err.response.status == 404) {
+            createErrorCard('No profile with this username')
+        }
+    }
+}
+
+
+async function getRepos(username) {
+    try{
+        const {data} = await axios(APIURL + username + '/repos?sort=created')
+        addReposToCard(data)
+    }
+    catch(err) {
+            createErrorCard('Problem fetching Repos') 
+    }
+}
+
+
+function createUserCard(user) {
+    const cardHTML =  `<div class="card">
+    <div>
+        <img src="${user.avatar_url}" class = "avatar">
+    </div>
+    <div class="user-info">
+        <h2>${user.name}</h2>
+        <p>${user.bio}</p>
+        <ul>
+            <li>${user.followers}<strong>Followers</strong></li>
+            <li>${user.following}<strong>Following</strong></li>
+            <li>${user.public_repos}<strong>Repos</strong></li>
+        </ul>
+
+        <div id="repos">
+           
+        </div>
+    </div>
+</div>
+`
+main.innerHTML = cardHTML
+
+}
+
+function addReposToCard(repos) {
+  const reposEL = document.getElementById("repos")
+  repos.slice(0,10)
+  .forEach(repo => {
+      const repoEl = document.createElement('a')
+      repoEl.classList.add('repos')
+      repoEl.href = repo.html_url
+      repoEl.target = '_blank'
+      repoEl.innerText = repo.name
+
+      reposEL.appendChild(repoEl)
+
+  });
+
+}
+
+function createErrorCard(msg) {
+const cardHTML = `
+<div class="card">
+<h1>${msg}</h1>
+</div>
+`
+main.innerHTML = cardHTML
+}
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const user = search.value
+
+    if(user) {
+        getUser(user)
+
+        search.value = ''
+    }
+})
